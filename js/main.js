0d05e8ca6b9116907dd3356f752bdf8c8a914478
Nikos Sklavounos <nikos.skl98@gmail.com>
2020-12-05 20:01:57 +0200

    Add NTUA & HUA

--- js/main.js
+++ js/main.js
@@ -1,29 +1,34 @@
 const loginButton = document.querySelector('#loginButton');
 const BASE_URL = 'https://unistudents-cb.herokuapp.com/api/student/'
 const messages = document.querySelector('.messages');
-const success = document.querySelector('#success');
 const wrongCr = document.querySelector('#wrong-cr');
 const systemDown = document.querySelector('#system-down');
 const problem = document.querySelector('#problem');
 const form = document.querySelector('form');
+const cards = document.querySelector('.cards');
+const infoCardBody = document.querySelector('#info > .card-body');
+const gradesCardBody = document.querySelector('#grades > .card-body');
 
 messages.style.display = 'none';
-success.style.display = 'none';
 wrongCr.style.display = 'none';
 systemDown.style.display = 'none';
 problem.style.display = 'none';
+cards.style.display = 'none';
+
 
 loginButton.addEventListener('click', (event) => {
     event.preventDefault();
     const university = document.querySelector('#universities').value;
     const username = document.querySelector('#username').value;
     const password = document.querySelector('#password').value;
-
+    const cookies = JSON.parse(localStorage.getItem("cookies"));
+    
     fetch(`${BASE_URL}${university}`, {
         method: "POST",
         body: JSON.stringify({
             username,
-            password
+            password,
+            cookies
         }),
         headers: { "Content-type": "application/json; charset=UTF-8" }
     })
@@ -31,7 +36,8 @@ loginButton.addEventListener('click', (event) => {
             if (response.status == 200) {
                 form.style.display = 'none';
                 messages.style.display = '';
-                success.style.display = '';
+                cards.style.display = '';
+                return response.json();
             } else if (response.status == 401) {
                 form.style.display = 'none';
                 messages.style.display = '';
@@ -45,5 +51,50 @@ loginButton.addEventListener('click', (event) => {
                 messages.style.display = '';
                 problem.style.display = '';
             }
-        });
+        })
+        .then(data => {
+            if (data) {
+                localStorage.setItem("cookies", JSON.stringify(data.cookies));
+                const info = data.student.info;
+                const grades = data.student.grades;
+    
+                infoCardBody.innerHTML = `
+                <p><b>Α.Μ:</b> ${info.aem}</p>
+                <p><b>Ονοματεπώνυμο:</b> ${info.lastName} ${info.firstName}</p>
+                <p><b>Τμήμα:</b> ${info.department}</p>
+                <p><b>Εξάμηνο:</b> ${info.semester}</p>
+                <p><b>Πρόγραμμα Σπουδών:</b> ${info.registrationYear}</p>
+                `
+    
+                gradesCardBody.innerHTML = `
+                <p><b>Μ.Ο (Σύνολο):</b> ${grades.totalAverageGrade}</p>
+                <p><b>Περασμένα (Σύνολο):</b> ${grades.totalPassedCourses}</p>
+                <p><b>ECTS (Σύνολο):</b> ${grades.totalEcts}</p>
+                `
+    
+                for (let i = 0; i < grades.semesters.length; i++) {
+                    const semester = grades.semesters[i];
+    
+                    gradesCardBody.innerHTML += `
+                    <hr>
+                    <p><b>ΕΞΑΜΗΝΟ </b> ${semester.id}</p>
+                    <p><b>Μ.Ο:</b> ${semester.gradeAverage}</p>
+                    <p><b>Περασμένα:</b> ${semester.passedCourses}</p>
+                    <p><b>ECTS:</b> ${semester.ects}</p>
+                    <p><i>Λίστα Μαθημάτων</i></p>
+                    `
+    
+                    for (let j = 0; j < semester.courses.length; j++) {
+                        const course = semester.courses[j];
+                        gradesCardBody.innerHTML += `
+                        <small><b>ID: </b>${course.id}</small>
+                        <small><b>Είδος: </b>${course.type}</small>
+                        <small><b>Περίοδος: </b>${course.examPeriod}</small>
+                        <p><b>${course.name}:</b> ${course.grade}</p>
+                        `
+                    }
+                }
+            }
+    
+        })
 })
